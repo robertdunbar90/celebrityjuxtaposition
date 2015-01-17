3e9eb10b7cb6b0432ada43f3ff84c75a6049d973
Robert Dunbar <robertdunbar90@hotmail.com>
2015-01-17 12:50:03 +0000

    pull info from page

--- cj.py
+++ cj.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,11 +8,17 @@ def index():
 @app.route('/_question')
 def question():
   return jsonify(question='Who is your favorite Knowles?',
-    answerA='Nick', answerB='Beyonce')
+    answerA='Nick', answerB='Beyonce', id=23)
 
 @app.route('/_answer')
 def answer():
-  return jsonify(aCount=3, bCount=2)
+  a = 1
+  b = 1
+  if request.args.get('answer') == 'a': a += 1
+  elif request.args.get('answer') == 'b': b += 1
+  aCount = 100*a/(a+b)
+  bCount = 100*b/(a+b)
+  return jsonify(aCount=aCount, bCount=bCount)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
