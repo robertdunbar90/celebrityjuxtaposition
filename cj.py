fec2df75dfe3624b928a85cb8b761cd4b687e84a
Robert Dunbar <robertdunbar90@hotmail.com>
2015-01-17 17:31:12 +0000

    added database

--- cj.py
+++ cj.py
@@ -1,5 +1,27 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, g
+import sqlite3
+
+DATABASE = '/tmp/cj.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+  return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_request():
+  g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+  db = getattr(g, 'db', None)
+  if db is not None:
+    db.close()
 
 @app.route('/')
 def index():
@@ -7,18 +29,29 @@ def index():
 
 @app.route('/_question')
 def question():
-  return jsonify(question='Who is your favorite Knowles?',
-    answerA='Nick', answerB='Beyonce', id=23)
+  v = g.db.execute('SELECT id, question, answerA, answerB FROM questions ORDER BY RANDOM() LIMIT 1;').fetchone()
+  q = jsonify(id=v[0], question=v[1], answerA=v[2], answerB=v[3])
+  return q
 
 @app.route('/_answer')
 def answer():
-  a = 1
-  b = 1
-  if request.args.get('answer') == 'a': a += 1
-  elif request.args.get('answer') == 'b': b += 1
+  i = request.args['id']
+  v = g.db.execute('SELECT aCount, bCount FROM questions WHERE id = ?', [i]).fetchone()
+  a = v[0]
+  b = v[1]
+  if request.args.get('answer') == 'a':
+    a += 1
+    g.db.execute('UPDATE questions SET aCount=? WHERE id=?;', [a, i])
+    g.db.commit()
+  elif request.args.get('answer') == 'b':
+    b += 1
+    g.db.execute('UPDATE questions SET bCount=? WHERE id=?;', [b, i])
+    g.db.commit()
   aCount = 100*a/(a+b)
   bCount = 100*b/(a+b)
   return jsonify(aCount=aCount, bCount=bCount)
 
+
+
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(host='0.0.0.0')
